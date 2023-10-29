e9e940fc78efd377ab453eaacb557c8fa4f2583e
velxc <velxmkm@gmail.com>
2023-10-29 16:25:10 +0200

    Verssion 9

---
+++ nn.py
@@ -0,0 +1,14 @@
+job_list = {
+    "Java developer": {"salary": 50, "gladness_less": 20},
+    "C++ developer": {"salary": 80, "gladness_less": 40},
+    "Python developer": {"salary": 40, "gladness_less": 60},
+    "Php developer": {"salary": 30, "gladness_less": 10},
+}
+
+
+brands_of_car = {
+    "BMW": {'fuel': 100, "strength": 100, 'consumption': 6},
+    "Ford": {'fuel': 80, "strength": 80, 'consumption': 8},
+    "Lada": {'fuel': 40, "strength": 20, 'consumption': 12},
+    "Volvo": {'fuel': 60, "strength": 150, 'consumption': 10}
+}
